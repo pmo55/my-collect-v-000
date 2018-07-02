a3d151b4ba31863e4a09134e58350fdd38226feb
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-02 20:28:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(array)
+  new_array=[]
   counter=0
   while counter < array.length
-  yield(array[counter])
+  new_array << yield(array[counter])
   counter+=1
 end
-
+new_array
 end
 
